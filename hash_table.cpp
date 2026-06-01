7ebd73113a1bf0b29e38a6fc585ccec0a210c812
riilanium <riilanium@gmail.com>
2026-06-01 23:28:55 +0300

    h

--- hash_table.cpp
+++ hash_table.cpp
@@ -1,17 +1,16 @@
 #include "HashTable.h"
-#include <functional>
+#include <stdexcept>
 
 HashTable::HashTable(size_t size) noexcept
-    : _capacity(static_cast<int32_t>(size)), _filled(0), table(_capacity) {
-}
-
+    : _capacity(static_cast<int32_t>(size)), _size(0), _table(size)
+{}
 HashTable::~HashTable() = default;
 
 size_t HashTable::hash_function(const KeyType &key) const {
     size_t hash = 0;
 
     for (char ch: key) {
-        hash = hash * 31 + static_cast<size_t>(ch);
+        hash = hash * 131 + static_cast<size_t>(ch);
     }
 
     return hash % _capacity;
@@ -22,10 +21,14 @@ void HashTable::insert(const KeyType &key, const ValueType &value) {
         int32_t new_capacity = _capacity * 2;
         std::vector<std::list<std::pair<KeyType, ValueType> > > new_table(new_capacity);
 
-        for (const auto &bucket: table) {
-            for (const auto &pair: bucket) {
-                size_t new_index = std::hash<KeyType>{}(pair.first) % new_capacity;
-                new_table[new_index].push_back(pair);
+        for (int32_t i = 0; i < _capacity; ++i) {
+            for (auto& pair : table[i]) {
+                int32_t old_capacity = _capacity;
+                _capacity = new_capacity;
+                size_t new_index = hash_function(pair.first);
+                _capacity = old_capacity;
+
+                new_table[new_index].push_back(std::move(pair));
             }
         }
 
@@ -71,28 +74,46 @@ void HashTable::remove(const KeyType &key) {
         }
     }
 }
-
-ValueType &HashTable::operator[](const KeyType &key) {
+ValueType& HashTable::operator[](const KeyType &key)
+{
     size_t index = hash_function(key);
 
-    for (auto &pair: table[index]) {
+
+    for (auto& pair : table[index]) {
         if (pair.first == key) {
             return pair.second;
         }
     }
 
-    insert(key, ValueType{});
-    _filled--;
+    if (getLoadFactor() > 0.75) {
 
-    for (auto &pair: table[index]) {
-        if (pair.first == key) {
-            return pair.second;
+        int32_t new_capacity = _capacity * 2;
+        std::vector<std::list<std::pair<KeyType, ValueType>>> new_table(new_capacity);
+
+
+        for (int32_t i = 0; i < _capacity; ++i) {
+            for (auto& pair : table[i]) {
+                int32_t old_capacity = _capacity;
+                _capacity = new_capacity;
+                size_t new_index = hash_function(pair.first);
+                _capacity = old_capacity;
+
+                new_table[new_index].push_back(std::move(pair));
+            }
         }
+
+        _capacity = new_capacity;
+        table = std::move(new_table);
+
+        index = hash_function(key);
     }
 
-    static ValueType default_value{};
-    return default_value;
+    table[index].push_back({key, ValueType{}});
+    _filled++;
+
+    return table[index].back().second;
 }
+
 
 double HashTable::getLoadFactor() {
     return static_cast<double>(_filled) / _capacity;
